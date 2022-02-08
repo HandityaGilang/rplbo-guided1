ae452afdc553f5470926f7307f303f33e2d3f3e2
HandityaGilang <79139938+HandityaGilang@users.noreply.github.com>
2022-02-08 15:42:41 +0700

    Create contoh71200537.java

---
+++ contoh71200537.java
@@ -0,0 +1,14 @@
+public class Contoh<NIM> {
+    public static void main(String args[]){
+        int x=10;
+        int y=25;
+        int z=x+y;
+
+        System.out.printIn("Hasil penjumlahan" + "dari x+y=" + z);
+        System.out.printIn("Hasil akar kuadrat" + "dari z = "+ Math.sqrt(z));
+        String namadepan = "Handitya";
+        System.out.printIn("Nama depan saya adalah = "+ namadepan);
+        System.out.printIn("Nama lengkap saya"+"adalah="+ namadepan + "Gilang");
+        
+    }
+}
